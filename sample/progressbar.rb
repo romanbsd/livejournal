47b04108b4834a6d3ddf3cf297f0813c8c0e41ca
Evan Martin <martine@danga.com>
2006-05-02 04:53:38 +0300

    split progressbar into own file
    darcs-hash:20060502015338-0faa6-01815a9e3f670cf62f30ff0c979b7b84bd978ede.gz

---
+++ sample/progressbar.rb
@@ -0,0 +1,41 @@
+class ProgressBar
+  def initialize(caption)
+    @cur = 0
+    @width = 40
+    print caption
+    print("[" + " "*40 + "]" + "\x08"*41)
+    $stdout.flush
+  end
+
+  def fill_to(pos)
+    dots = pos - @cur
+    print "."*dots
+    $stdout.flush
+    @cur = pos
+  end
+
+  def update(cur, max)
+    fill_to(@width*cur/max)
+  end
+
+  def finish(error=false)
+    return unless @cur >= 0
+    fill_to(@width) unless error
+    puts
+    @cur = -1
+  end
+
+  def self.with_progress(caption)
+    bar = ProgressBar.new(caption)
+    begin
+      yield bar
+    rescue => e
+      bar.finish(true)
+      raise e
+    else
+      bar.finish(false)
+    end
+  end
+end
+
+# vim: ts=2 sw=2 et :
